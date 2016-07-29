379c4e49ff5a6d0b6a03d239fc71ade6cbe14bcb
Brunno Goncalves <brunnocgoncalves@gmail.com>
2016-07-29 13:52:42 -0300

    Inicial commit

--- exercise05/ViewController.swift
+++ exercise05/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headerImage: UIImageView!
+    @IBOutlet weak var multipleText: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    
+    @IBOutlet weak var multipleLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var multiple: Int = 0
+    let times: Int = 3
+    var result: Int = 0
+    var addTimes: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +32,40 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func startGame(sender: UIButton) {
+        if multipleText.text != nil{
+            headerImage.hidden = true
+            multipleText.hidden = true
+            playButton.hidden = true
+            
+            multipleLabel.hidden = false
+            addButton.hidden = false
+            
+            multiple = Int(multipleText.text!)!
+        }
+    }
 
 
+    @IBAction func add(sender: UIButton) {
+        if addTimes < times{
+            result = addTimes + multiple
+            multipleLabel.text = "\(addTimes) + \(multiple) = \(result)"
+            addTimes += 1
+        }
+        else{
+            headerImage.hidden = false
+            multipleText.hidden = false
+            playButton.hidden = false
+            
+            multipleLabel.hidden = true
+            addButton.hidden = true
+            
+            addTimes = 0
+            multipleText.text = nil
+            multipleLabel.text = "Press ADD to add!"
+        }
+    }
 }
 
